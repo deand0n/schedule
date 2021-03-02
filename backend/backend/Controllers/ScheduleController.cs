65ba8d43c128899a6283dd1227d9d9a8c0c99ba2
deand0n <vladuslavkopulec@gmail.com>
2021-03-02 23:58:13 +0200

    Added parser and temporary model

---
+++ backend/backend/Controllers/ScheduleController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class ScheduleController : Controller
+    {
+        // TODO move this variable from here
+        private static readonly HttpClient client = new HttpClient();
+        
+        
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            
+            var values = new Dictionary<string, string>
+            {
+                { "faculty", "1004" },
+                {"group", "DS-1910"},
+                {"sdate", "01.03.2021"},
+                {"edate","07.03.2021"}
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            // send post request to timetable and save it to "response"
+            var response =  await client.PostAsync("http://195.95.232.162:8082/cgi-bin/timetable.cgi?n=700", content);
+
+            // encode response using "windows 1251"
+            var buffer = await response.Content.ReadAsByteArrayAsync();
+            var byteArray = buffer.ToArray();
+            var responseString = Encoding.GetEncoding(1251).GetString(byteArray, 0, byteArray.Length);
+
+            
+            return responseString;
+        }
+    }
+}
---
+++ backend/backend/Models/Lesson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace backend.Models
+{
+    public class Lesson
+    {
+        public int LessonNumber { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+    }
+}
