854935235acdf591b7058f92bf938f457e1cc2fe
deand0n <vladuslavkopulec@gmail.com>
2021-03-12 14:14:22 +0200

    added new field to class Lesson called "Groups"

--- backend/backend/Models/Lesson.cs
+++ backend/backend/Models/Lesson.cs
@@ -4,13 +4,11 @@ namespace backend.Models
 {
     public class Lesson
     {
-        public int LessonNumber { get; set; }
-        // Time[0] - start time
-        // Time[1] - end time
-        public string[] Time { get; set; } = new string[2]; 
+        public string OrdinalNumber { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
         public string Type { get; set; }
         public string Name { get; set; }
-        public string Teacher { get; set; }
-        public string Location { get; set; }
+        public string Groups { get; set; }
     }
 }
