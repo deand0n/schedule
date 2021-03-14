cc4991fd61d0dbfe09f822fc795a9cb440102cf8
deand0n <vladuslavkopulec@gmail.com>
2021-03-15 00:00:41 +0200

    added encoding converter for "values"

--- backend/backend/Controllers/ScheduleController.cs
+++ backend/backend/Controllers/ScheduleController.cs
@@ -25,14 +25,37 @@ namespace backend.Controllers
             var values = new Dictionary<string, string>
             {
                 { "faculty", "1004" },
-                {"group", "DS-1910"},
+                {"teacher", ""},
+                {"group", "%CF%C7-2004"},
                 {"sdate", "01.03.2021"},
                 {"edate","28.03.2021"}
             };
-
+            
+            
+            /*
+            Encoding utf8 = Encoding.UTF8;
+            Encoding win1251 = Encoding.GetEncoding(1251);
+            string sss = values["group"];
+            byte[] utf8Bytes = utf8.GetBytes(sss.Substring(0, sss.IndexOf("-")));
+            byte[] win1251Bytes = Encoding.Convert(utf8, win1251, utf8Bytes);
+            
+            string hex = BitConverter.ToString(win1251Bytes);
+            hex = hex.Replace("-", "%");
+            hex += sss.Substring(sss.IndexOf("-"), sss.Length-2);
+            hex = hex.Insert(0, "%");
+            Console.WriteLine(hex);
+            values["group"] = hex;
+            Console.WriteLine(values["group"]);
+            //string encodedString = win1251.GetString(win1251Bytes);
+            */
+            
+            
+            
+            
             var content = new FormUrlEncodedContent(values);
             // send POST request to timetable and save it to "response"
             var response =  await Client.PostAsync("http://195.95.232.162:8082/cgi-bin/timetable.cgi?n=700", content);
+            
             // encode response using "windows 1251"
             var buffer = await response.Content.ReadAsByteArrayAsync();
             var byteArray = buffer.ToArray();
@@ -51,8 +74,6 @@ namespace backend.Controllers
             var startTimeRegex = new Regex(@"<td>([0-9]{2}\:[0-9]{2})<br>");
             var endTimeRegex = new Regex(@"<br>([0-9]{2}\:[0-9]{2})</td>");
             var nameRegex = new Regex(@"[0-9]{2}\:[0-9]{2}</td><td>(.*?)</td></tr>");
-            
-            
             
             var dayMatches = dayRegex.Matches(responseString);
             int dayCounter = dayMatches.Count;
@@ -87,8 +108,10 @@ namespace backend.Controllers
                     days[i].Lessons[j].StartTime = startTimeRegex.Matches(dayMatches[i].Value)[j].Groups[1].Value;
                     days[i].Lessons[j].EndTime = endTimeRegex.Matches(dayMatches[i].Value)[j].Groups[1].Value;
                     
+                    // create temporary variables 
                     string tempName = nameRegex.Matches(dayMatches[i].Value)[j].Groups[1].Value;
                     string tempType, tempGroups;
+                    // if there are empty lesson, assign temporary variables to null
                     if (tempName == " ")
                     {
                         tempName = null;
@@ -100,26 +123,32 @@ namespace backend.Controllers
                         Regex typeRegex = new Regex(@"\((.*?)\)<br> ");
                         tempType = typeRegex.Match(tempName).Groups[1].Value;
                         
+                        // remove junk from name of the lesson
                         tempName = tempName.Replace(typeRegex.Match(tempName).Value, "");
-                        tempName = tempName.Replace("\u00A0", " ");
                         tempName = tempName.Replace("<br>  <div class='link'> </div> ", "");
+                        tempName = tempName.Replace("\u00A0", " ");
 
-                        tempGroups = "DS-1910";
+                        tempGroups = null;
+                        // check if the lesson is only for one group
                         if (tempName.Contains("<br>"))
                         {
+                            // if there are more than one group
+                            // fill 'tempGroups' variable and remove it from 'tempName'  
                             Regex groupsRegex = new Regex("<br> (.*?)<br> <div class='link'> </div>");
                             tempGroups = groupsRegex.Match(tempName).Groups[1].Value;
 
                             tempName = tempName.Replace(groupsRegex.Match(tempName).Value, "");
                         }
-                        
                     }
-                    days[i].Lessons[j].Name = tempName;
                     days[i].Lessons[j].Type = tempType;
+                    days[i].Lessons[j].Name = tempName;
                     days[i].Lessons[j].Groups = tempGroups;
                 }
             }
             return days;
         }
+        
+        
+        
     }
 }
